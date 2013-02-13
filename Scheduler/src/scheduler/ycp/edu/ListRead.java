9fd6c71f7214cd3925de8bdd2e1ab71b71b620a5
Amanda Miller <amille61@ycp.edu>
2013-02-13 11:43:48 -0500

    Starting CSV Reading

---
+++ Scheduler/src/scheduler/ycp/edu/ListRead.java
@@ -0,0 +1,15 @@
+package scheduler.ycp.edu;
+
+import java.io.BufferedReader;
+
+public class ListRead {
+	public static void main(String[] arg){
+		BufferedReader list = new Buffered
+		courseList = new ArrayList();
+		
+		String courseChoice = list.readLine();
+		while{
+			
+		}
+	}
+}
