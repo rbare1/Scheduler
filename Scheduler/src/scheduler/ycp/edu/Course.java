440a40fad5aea907aa2c0f6815d3cad537189ec9
Amanda Miller <amille61@ycp.edu>
2013-02-14 13:56:07 -0500

    Added Course Class

---
+++ Scheduler/src/scheduler/ycp/edu/Course.java
@@ -0,0 +1,13 @@
+package scheduler.ycp.edu;
+
+public class Course {
+	public String num;
+	public int CRNNum;
+	public String name;
+	public int credit;
+	public String days;
+	public double startTime;
+	public double endTime;
+	public String instructor;
+	public String room;
+}
