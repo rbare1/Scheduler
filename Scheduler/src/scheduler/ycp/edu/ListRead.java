cd5a7ff94d9aea73e05eae144844d789c5510f40
Amanda Miller <amille61@ycp.edu>
2013-02-15 09:25:33 -0500

    List Read Test

--- Scheduler/src/scheduler/ycp/edu/ListRead.java
+++ Scheduler/src/scheduler/ycp/edu/ListRead.java
@@ -8,24 +8,32 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class ListRead {
-	public static void main(String[] arg) throws IOException{
-		File fileName = new File("Course List.csv");
-		BufferedReader list = new BufferedReader(new FileReader(fileName));
-		String courseChoice = list.readLine();
-		ArrayList<Course> courses = new ArrayList<Course>();
-		while(courseChoice!="NULL"){
-			Course course = new Course();
-			StringTokenizer description = new StringTokenizer(courseChoice,",");
-			course.num = description.nextToken(); 
-			course.CRNNum = Integer.valueOf(description.nextToken());
-			course.name = description.nextToken();
-			course.credit = description.nextToken();
-			course.days = description.nextToken();
-			course.startTime = Double.parseDouble(description.nextToken());
-			course.endTime = Double.parseDouble(description.nextToken());
-			course.instructor = description.nextToken();
-			course.room = description.nextToken();
-			courses.add(course);
-		}
+	
+	File fileName;
+	
+	public ListRead(File Name){
+		fileName = Name;
+	}
+	
+	public ArrayList Read() throws IOException{
+	File fileName = new File("fileName");
+	BufferedReader list = new BufferedReader(new FileReader(fileName));
+	ArrayList<Course> courses = new ArrayList<Course>();
+	String courseChoice = list.readLine();
+	while(courseChoice!="NULL"){
+		Course course = new Course();
+		StringTokenizer description = new StringTokenizer(courseChoice,",");
+		course.num = description.nextToken(); 
+		course.CRNNum = Integer.valueOf(description.nextToken());
+		course.name = description.nextToken();
+		course.credit = description.nextToken();
+		course.days = description.nextToken();
+		course.startTime = Double.parseDouble(description.nextToken());
+		course.endTime = Double.parseDouble(description.nextToken());
+		course.instructor = description.nextToken();
+		course.room = description.nextToken();
+		courses.add(course);
+	}
+	return courses;
 	}
 }
