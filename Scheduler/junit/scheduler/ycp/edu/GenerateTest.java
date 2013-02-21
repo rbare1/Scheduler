c3b3bb1f8e2ce91f1365843f784cc32d89b3c83a
Ryan Bare <Ryan Bare@192.168.0.185>
2013-02-21 00:30:56 -0500

    worked more on the generate class having problems with .contains not working also created the ScheduleViewer class but  the GWT designer won't work

--- Scheduler/junit/scheduler/ycp/edu/GenerateTest.java
+++ Scheduler/junit/scheduler/ycp/edu/GenerateTest.java
@@ -3,6 +3,7 @@ package scheduler.ycp.edu;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,37 +17,47 @@ public class GenerateTest {
 		private Course course3 = new Course(null, 0, null, 0, null, 0, 0, null, null);
 		private Course course4 = new Course(null, 0, null, 0, null, 0, 0, null, null);
 		private Course course5 = new Course(null, 0, null, 0, null, 0, 0, null, null);
-		ArrayList<Course> scheduleM = new ArrayList<Course>();
-		ArrayList<Course> scheduleT = new ArrayList<Course>();
-		ArrayList<Course> scheduleW = new ArrayList<Course>();
-		ArrayList<Course> scheduleR = new ArrayList<Course>();
-		ArrayList<Course> scheduleF = new ArrayList<Course>();
+		private List<Course> scheduleM = new ArrayList<Course>();
+		private List<Course> scheduleT = new ArrayList<Course>();
+		private List<Course> scheduleW = new ArrayList<Course>();
+		private List<Course> scheduleR = new ArrayList<Course>();
+		private List<Course> scheduleF = new ArrayList<Course>();
 
+		Generate generate = new Generate();
 		@Before
 		public void setUp() {
 		// create test fixture objects
 			course1.setNum("CS101");
 			course1.setDays("MWF");
-			scheduleM.add(course1);
-			scheduleW.add(course1);
+			course1.setStartTime(9.0);
+			course1.setEndTime(10.15);
 			
 			course2.setNum("ART101");
 			course2.setDays("MW");
-			scheduleM.add(course2);
-			scheduleW.add(course2);
+			course2.setStartTime(11.0);
+			course2.setEndTime(12.00);
 			
 			course3.setNum("PE101");
 			course3.setDays("F");
-			scheduleF.add(course3);
-			
+			course3.setStartTime(9.0);
+			course3.setEndTime(10.15);
+					
 			course4.setNum("ECE101");
 			course4.setDays("TR");
-			scheduleT.add(course4);
-			scheduleR.add(course4);
-			
+			course4.setStartTime(3.0);
+			course4.setEndTime(4.15);
+				
 			course5.setNum("PHY101");
 			course1.setDays("R");
-			scheduleR.add(course5);
+			course1.setStartTime(1.0);
+			course1.setEndTime(2.0);
+			
+			generate.GenerateSchedule(course1, course2, course3, course4, course5);
+			scheduleM = generate.getScheduleM();
+			scheduleT = generate.getScheduleT();
+			scheduleW = generate.getScheduleW();
+			scheduleR = generate.getScheduleR();
+			scheduleF = generate.getScheduleF();
 			
 		}
 		
@@ -54,24 +65,26 @@ public class GenerateTest {
 	public void testGetScheduleM() throws Exception {	
 		 assertEquals("CS101", scheduleM.get(0).getNum());	
 		 assertEquals("ART101", scheduleM.get(1).getNum());	
+		 assertEquals(3, scheduleM.size());
+		 
 		}
-	
+	//@Test
 	public void testGetScheduleT() throws Exception {	
 		 assertEquals("ECE101", scheduleT.get(0).getNum());		
 		}
-	
+	//@Test
 	public void testGetScheduleW() throws Exception {	
 		assertEquals("CS101", scheduleW.get(0).getNum());	
 		 assertEquals("ART101", scheduleW.get(1).getNum());	
 		}
-	
+	//@Test
 	public void testGetScheduleR() throws Exception {	
 		 assertEquals("ECE101", scheduleR.get(0).getNum());	
-		 assertEquals("PHY101", scheduleR.get(0).getNum());	
+		 assertEquals("PHY101", scheduleR.get(1).getNum());	
 		}
-	
+	//@Test
 	public void testGetScheduleF() throws Exception {	
-		 assertEquals("CS101", scheduleF.get(0).getNum());	
+		 assertEquals("PE101", scheduleF.get(0).getNum());	
 		}
 
 }
--- Scheduler/src/scheduler/ycp/edu/Generate.java
+++ Scheduler/src/scheduler/ycp/edu/Generate.java
@@ -5,26 +5,22 @@ import java.util.List;
 
 
 public class Generate { 
+	private static List<Course> scheduleM = new ArrayList<Course>();
+	private static List<Course> scheduleT = new ArrayList<Course>();
+	private static List<Course> scheduleW = new ArrayList<Course>();
+	private static List<Course> scheduleR = new ArrayList<Course>();
+	private static List<Course> scheduleF = new ArrayList<Course>();
 	
-	ArrayList<Course> scheduleM = new ArrayList<Course>();
-	ArrayList<Course> scheduleT = new ArrayList<Course>();
-	ArrayList<Course> scheduleW = new ArrayList<Course>();
-	ArrayList<Course> scheduleR = new ArrayList<Course>();
-	ArrayList<Course> scheduleF = new ArrayList<Course>();
-	public void GenerateSchedule(){
+	boolean checkOpen = false;
 
-		Course course1 = new Course(null, 0, null, 0, null, 0, 0, null, null);		// course 1 is most important
-		Course course2 = new Course(null, 0, null, 0, null, 0, 0, null, null);		// course 2 is second, etc
-		Course course3 = new Course(null, 0, null, 0, null, 0, 0, null, null);
-		Course course4 = new Course(null, 0, null, 0, null, 0, 0, null, null);
-		Course course5 = new Course(null, 0, null, 0, null, 0, 0, null, null);
-		
+	
+	public void GenerateSchedule(Course course1, Course course2, Course course3, Course course4, Course course5){
 		
 		int numCourses = 5;
 		int cnt = 0;
 		Course course;
 		
-		for(int i =0; i < numCourses; i++){		// cycles through each course to add to the appropriate list
+		for(int i = 0; i < numCourses; i++){		// cycles through each course to add to the appropriate list
 			if(cnt == 0){
 				course = course1;
 			}
@@ -42,28 +38,149 @@ public class Generate {
 			}
 			cnt++;
 			
-			if(course.getDays().contains("M")){		// separates classes by the day of the week they are in
-				scheduleM.add(course);				// classes can be in separate days
+			// separates classes by the day of the week they are in
+			// classes can be in multiple days
+
+			if(course.getDays() == "M"){	// .contains is not working for some reason
+											// it is the cause of null pointer exceptions
+											// since the test tries to access elements of an empty array
+				checkOpen = checkScheduleM(course);
+				if(checkOpen){
+					scheduleM.add(course);	
+				}
 			}
 			if(course.getDays().contains("T")){
-				scheduleT.add(course);
+				checkOpen = checkScheduleT(course);
+				if(checkOpen){
+					scheduleT.add(course);
+				}	
 			}
 			if(course.getDays().contains("W")){
-				scheduleW.add(course);
+				checkOpen = checkScheduleW(course);
+				if(checkOpen){
+					scheduleW.add(course);
+				}
 			}
 			if(course.getDays().contains("R")){
-				scheduleR.add(course);
+				checkOpen = checkScheduleT(course);
+				if(checkOpen){
+					scheduleR.add(course);
+				}
 			}
 			if(course.getDays().contains("F")){
-				scheduleF.add(course);
+				checkOpen = checkScheduleF(course);
+				if(checkOpen){
+					scheduleF.add(course);
+				}
 			}
-			
+			scheduleM.add(course1);
 		}
 		
 	}
 	public List<Course> getScheduleM(){
-		GenerateSchedule();
+	//	GenerateSchedule();
 		return scheduleM;
+	}
+	public List<Course> getScheduleT(){
+		//GenerateSchedule();
+		return scheduleT;
+	}
+	public List<Course> getScheduleW(){
+		//GenerateSchedule();
+		return scheduleW;
+	}
+	public List<Course> getScheduleR(){
+		//GenerateSchedule();
+		return scheduleR;
+	}
+	public List<Course> getScheduleF(){
+		//GenerateSchedule();
+		return scheduleF;
+	}
+	
+	public boolean checkScheduleM(Course course){
+		for(int j = 0; j < scheduleM.size(); j ++){
+			if(course.getStartTime() >= scheduleM.get(j).getStartTime() && course.getStartTime() <= scheduleM.get(j).getEndTime()){
+				checkOpen = false;	// checkOpens if other classes already use start time slot
+				break;	// break so checkOpen isn't overridden by checkOpening another class
+			}
+			else if(course.getEndTime() >= scheduleM.get(j).getStartTime() && course.getEndTime() <= scheduleM.get(j).getEndTime()){
+				checkOpen = false;	// checkOpen if other classes already use end time slot
+				break;
+			}
+			else{
+				checkOpen = true;
+			}
+		}
+		return checkOpen;
+	}
+	
+	public boolean checkScheduleT(Course course){
+		for(int j = 0; j < scheduleT.size(); j ++){
+			if(course.getStartTime() >= scheduleT.get(j).getStartTime() && course.getStartTime() <= scheduleT.get(j).getEndTime()){
+				checkOpen = false;	// checkOpens if other classes already use start time slot
+				break;	// break so checkOpen isn't overridden by checkOpening another class
+			}
+			else if(course.getEndTime() >= scheduleT.get(j).getStartTime() && course.getEndTime() <= scheduleT.get(j).getEndTime()){
+				checkOpen = false;	// checkOpen if other classes already use end time slot
+				break;
+			}
+			else{
+				checkOpen = true;
+			}
+		}
+		return checkOpen;
+	}
+	
+	public boolean checkScheduleW(Course course){
+		for(int j = 0; j < scheduleW.size(); j ++){
+			if(course.getStartTime() >= scheduleW.get(j).getStartTime() && course.getStartTime() <= scheduleW.get(j).getEndTime()){
+				checkOpen = false;	// checkOpens if other classes already use start time slot
+				break;	// break so checkOpen isn't overridden by checkOpening another class
+			}
+			else if(course.getEndTime() >= scheduleW.get(j).getStartTime() && course.getEndTime() <= scheduleW.get(j).getEndTime()){
+				checkOpen = false;	// checkOpen if other classes already use end time slot
+				break;
+			}
+			else{
+				checkOpen = true;
+			}
+		}
+		return checkOpen;
+	}
+	
+	public boolean checkScheduleR(Course course){
+		for(int j = 0; j < scheduleR.size(); j ++){
+			if(course.getStartTime() >= scheduleR.get(j).getStartTime() && course.getStartTime() <= scheduleR.get(j).getEndTime()){
+				checkOpen = false;	// checkOpens if other classes already use start time slot
+				break;	// break so checkOpen isn't overridden by checkOpening another class
+			}
+			else if(course.getEndTime() >= scheduleR.get(j).getStartTime() && course.getEndTime() <= scheduleR.get(j).getEndTime()){
+				checkOpen = false;	// checkOpen if other classes already use end time slot
+				break;
+			}
+			else{
+				checkOpen = true;
+			}
+		}
+		return checkOpen;
+	}
+	
+	public boolean checkScheduleF(Course course){
+		for(int j = 0; j < scheduleF.size(); j ++){
+			if(course.getStartTime() >= scheduleF.get(j).getStartTime() && course.getStartTime() <= scheduleF.get(j).getEndTime()){
+				checkOpen = false;	// checkOpens if other classes already use start time slot
+				break;	// break so checkOpen isn't overridden by checkOpening another class
+			}
+			else if(course.getEndTime() >= scheduleF.get(j).getStartTime() && course.getEndTime() <= scheduleF.get(j).getEndTime()){
+				checkOpen = false;	// checkOpen if other classes already use end time slot
+				break;
+			}
+			else{
+				checkOpen = true;
+			}
+		}
+		return checkOpen;
 	}
 	
 }
