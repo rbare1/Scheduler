7ede6ed5e8dd737c24f42e9ed2fbab82001a012e
Ryan Bare <rbare1@ycp.edu>
2013-02-22 11:15:16 -0500

    change package name of prob domain classes so UI can use
    Added prob domain classes project to build path for UI project.

---
+++ Scheduler/src/scheduler/ycp/edu/shared/Course.java
@@ -0,0 +1,86 @@
+package scheduler.ycp.edu.shared;
+
+
+public class Course {
+	private String num;
+	private int CRNNum;
+	private String name;
+	private int credit;
+	private String days;
+	private double startTime;
+	private double endTime;
+	private String instructor;
+	private String room;
+	
+	public Course(String num2, int CRNNum2, String name2, int credit2, String days2, double startTime2, double endTime2, String instructor2, String room2) {
+		setNum(num2);
+		setCRNNum(CRNNum2);
+		setName(name2);
+		setCredit(credit2);
+		setDays(days2);
+		setStartTime(startTime2);
+		setEndTime(endTime2);
+		setInstructor(instructor2);
+		setRoom(room2);		
+	}
+	
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public int getCRNNum() {
+		return CRNNum;
+	}
+	public void setCRNNum(int cRNNum) {
+		CRNNum = cRNNum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCredit() {
+		return credit;
+	}
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+	public String getDays() {
+		return days;
+	}
+	public void setDays(String days) {
+		this.days = days;
+	}
+	public double getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(double startTime) {
+		this.startTime = startTime;
+	}
+	public double getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(double endTime) {
+		this.endTime = endTime;
+	}
+	public String getInstructor() {
+		return instructor;
+	}
+	public void setInstructor(String instructor) {
+		this.instructor = instructor;
+	}
+	public String getRoom() {
+		return room;
+	}
+	public void setRoom(String room) {
+		this.room = room;
+	}
+
+}
+
+
+
+
---
+++ Scheduler/src/scheduler/ycp/edu/shared/IDatabase.java
@@ -0,0 +1,5 @@
+package scheduler.ycp.edu.shared;
+
+public interface IDatabase {
+	public Course findCourse(String courseName);
+}
