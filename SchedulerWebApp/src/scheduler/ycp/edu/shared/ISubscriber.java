0c41ed3c90783caa76581c8d058033b1bf7d6121
Ryan Bare <Ryan Bare@192.168.37.216>
2013-02-27 11:56:40 -0500

    added web app sub/pub

---
+++ SchedulerWebApp/src/scheduler/ycp/edu/shared/ISubscriber.java
@@ -0,0 +1,18 @@
+package scheduler.ycp.edu.shared;
+
+import IPublisher;
+import Object;
+
+/**
+ * Interface implemented by classes that are event subscribers.
+ */
+public interface ISubscriber {
+	/**
+	 * Called by a Publisher when an event occurs.
+	 * 
+	 * @param key        key indicating the type of the event
+	 * @param publisher  the publisher
+	 * @param hint       additional information about the event
+	 */
+	public void eventOccurred(Object key, IPublisher publisher, Object hint);
+}
---
+++ SchedulerWebApp/src/scheduler/ycp/edu/shared/Publisher.java
@@ -0,0 +1,77 @@
+package scheduler.ycp.edu.shared;
+
+import ArrayList;
+import IPublisher;
+import ISubscriber;
+import Iterator;
+import List;
+import Object;
+import Override;
+import Publisher.Registration;
+
+public abstract class Publisher implements IPublisher {
+	private static class Registration {
+		Object key;
+		ISubscriber subscriber;
+		
+		Registration(Object key, ISubscriber subscriber) {
+			this.key = key;
+			this.subscriber = subscriber;
+		}
+	}
+	
+	private List<Registration> registrationList;
+	
+	/**
+	 * Constructor.
+	 */
+	protected Publisher() {
+		registrationList = new ArrayList<Registration>();
+	}
+	
+	@Override
+	public void subscribe(Object key, ISubscriber subscriber) {
+		registrationList.add(new Registration(key, subscriber));
+	}
+	
+	@Override
+	public void subscribeToAll(Object[] keyList, ISubscriber subscriber) {
+		for (Object key : keyList) {
+			registrationList.add(new Registration(key, subscriber));
+		}
+	}
+	
+	@Override
+	public void unsubscribe(Object key, ISubscriber subscriber) {
+		for (Iterator<Registration> i = registrationList.iterator(); i.hasNext(); ) {
+			Registration reg = i.next();
+			if (reg.key.equals(key) && reg.subscriber == subscriber) {
+				i.remove();
+				return;
+			}
+		}
+	}
+	
+	@Override
+	public void unsubscribeFromAll(ISubscriber subscriber) {
+		for (Iterator<Registration> i = registrationList.iterator(); i.hasNext(); ) {
+			Registration reg = i.next();
+			if (reg.subscriber == subscriber) {
+				i.remove();
+			}
+		}
+	}
+	
+	@Override
+	public void notifySubscribers(Object key, Object hint) {
+		// protect against concurrent modification exceptions
+		ArrayList<Registration> registrationListCopy = new ArrayList<Registration>(registrationList);
+		
+		// notify all subscribers subscribed for events with this key
+		for (Registration reg : registrationListCopy) {
+			if (reg.key.equals(key)) {
+				reg.subscriber.eventOccurred(key, this, hint);
+			}
+		}
+	}
+}
